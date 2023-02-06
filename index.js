026f3f1a1ee64474f03bf7e2d16008b77175db13
MrKamex <oli.droege@web.de>
2023-02-06 18:16:03 +0100

    test

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+const fakten = ["Das hier ist Fakt 1","Das hier ist Fakt 2","Das hier ist Fakt 3","Das hier ist Fakt 4","Das hier ist Fakt 5","Das hier ist Fakt 6"]
+
+let current = fakten.clone();
+
 app.get('/', async (req, res) => {
   const username = req.query.username || 'myogeshchavan97';
   try {
@@ -19,6 +23,29 @@ app.get('/', async (req, res) => {
       .sort((a, b) => b.stars - a.stars);
 
     res.send(repos);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
+  }
+});
+
+app.get('/getFact', async (req, res) => {
+  const username = req.query.username || 'myogeshchavan97';
+  try {
+	 
+	const index = Math.floor(Math.random() * current.length)
+	const result = current[index];
+		current.splice(index,1);
+    res.send(result);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
+  }
+});
+
+app.get('/resetFacts', async (req, res) => {
+  const username = req.query.username || 'myogeshchavan97';
+  try {
+    current = fakten.clone();
+    res.send('OK');
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
